acf20edd3d265e6d388967342c3a18c426e9e624
unknown <adamc17@.vrac.iastate.edu>
2011-06-28 15:30:38 -0500

    commit while playing with git pushing

--- Factory/adam_factory3.lua
+++ Factory/adam_factory3.lua
@@ -93,7 +93,6 @@ rollerGroup = Group{
 	},
 	
 }
-
 rollerGroup2 = Group{
 	Transform {
 		position = {-1.5,0,-13.5},
@@ -140,7 +139,6 @@ rollerGroup2 = Group{
 		roller2,
 	}
 }
-
 palletGroup = Group{
 	Transform{
 		position = {-9,0,-2.4},
@@ -175,10 +173,9 @@ palletGroup = Group{
 		Pallet,
 	},	
 }
-
 HVACGroup = Group{
 Transform{
-		position = {-3.2,7,-10},,
+		position = {-3.2,7,-10},
 		HVAC,
 	},
 Transform{
@@ -214,9 +211,116 @@ Transform{
 		HVAC,
 	},	
 }	
+DiffuserGroup = Group{
+	Transform{
+	position = {-3.2,6.5,-9},
+	Diffuser,
+},
+	Transform{
+	position = {-3.2,6.5,-4},
+	Diffuser,
+},
+	Transform{
+	position = {-3.2,6.5,1},
+	Diffuser,
+},
+	Transform{
+	position = {3.7,6.5,-4},
+	Diffuser,
+},
+	Transform{
+	position = {3.7,6.5,1},
+	Diffuser,
+},
+	Transform{
+	position = {10.6,6.5,-9},
+	Diffuser,
+},
+	Transform{
+	position = {10.6,6.5,-4},
+	Diffuser,
+},
+	Transform{
+	position = {10.6,6.5,1},
+	Diffuser,
+},
+	}
+WorkbenchGroup = Group{
+	Transform{
+	position = {3,0,-10},
+	Workbench,
+},
+	Transform{
+	position = {3,0,-6},
+	Workbench,
+},
+	Transform{
+	position = {3,0,-2},
+	Workbench,
+},
+	Transform{
+	position ={7,0,-13},
+	Workbench,
+},
+	Transform{
+	position = {11.2,0,-13},
+	Workbench,
+},
+	Transform{
+	position ={11.2,0,-9},
+	Workbench,
+},
+	Transform{
+	position =  {11.2,0,-5},
+	Workbench,
+},
+}
+BeamGroup = Group{
+	Transform{
+		position = {-1.6,0,-2},
+		Beam,
+	},	
+Transform{
+		position = {-1.6,0,-9},
+		Beam,
+	},	
+Transform{
+		position = {8.4,0,-9},
+		Beam,
+	},
+Transform{
+		position = {8.4,0,-2},
+		Beam,
+	},
+}		
+	BarrelGroup = Group{
+	Transform{
+		position = {-9,.0,-7},
+		Barrel,
+},
+	Transform{
+		position = {-9,0,-5.7},
+		Barrel,
+},
+	Transform{
+		position ={-9,0,-4.4},
+		Barrel,
+},
+	Transform{
+		position ={-7.7,0,-7},
+		Barrel,
+},
+	Transform{
+		position =  {-7.7,0,-5.7},
+		Barrel,
+},
+	Transform{
+		position = {-7.7,0,-4.4},
+		Barrel,
+},
+}
 
 
-	
 robot = Transform {
 	position = {-3,0,-12},
 	Model("Factory Models/OSG/Machines/Robot.osg"),
@@ -226,10 +330,7 @@ robot2 = Transform {
 	--orientation = AngleAxis(Degrees(180), Axis{0.0,1.0,0.0}),
 	Model("Factory Models/OSG/Machines/Robot.osg"),
 }
-robot3 = Transform {
-	position = {7.5,0,-5},
-	Model("Factory Models/OSG/Machines/Robot.osg"),
-}	
+	
 cnc = Transform{
 	position = {-2,0,-4},
 	orientation = AngleAxis(Degrees(-90), Axis{0.0,1.0,0.0}),
@@ -270,41 +371,6 @@ Compressor =Transform{
 	--orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
 	Model("Factory Models/OSG/Tools/air compressor.osg"),
 }
-Workbench = Transform{
-	position = {3,0,-10},
-	orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Workbench/Work Bench.osg"),
-}	
-Workbench2 = Transform{
-	position = {3,0,-6},
-	orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Workbench/Work Bench.osg"),
-}	
-Workbench3 = Transform{
-	position = {3,0,-2},
-	orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Workbench/Work Bench.osg"),
-}	
-Workbench4 = Transform{
-	position = {7,0,-13},
-	--orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Workbench/Work Bench.osg"),
-}	
-Workbench5 = Transform{
-	position = {11.2,0,-13},
-	orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Workbench/Work Bench.osg"),
-}	
-Workbench6 = Transform{
-	position = {11.2,0,-9},
-	orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Workbench/Work Bench.osg"),
-}	
-Workbench7 = Transform{
-	position = {11.2,0,-5},
-	orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Workbench/Work Bench.osg"),
-}	
 ToolBox =Transform{
 	position = {5.5,0,-6},
 	orientation = AngleAxis(Degrees(180), Axis{0,1,0}),
@@ -330,7 +396,6 @@ BandSaw = Transform{
 	orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
 	Model("Factory Models/OSG/Machines/bandsaw.osg"),
 }
-
 RP = Transform{
 	position = {4.2,0,-10},
 	orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
@@ -371,121 +436,26 @@ ForkliftPath =Transform{
 	--orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
 	Model("Factory Models/OSG/Shop Carts and Fort Lifts/Path.osg"), 
 }	
-Barrel =Transform{
-	position = {-9,.0,-7},
-	--orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Storage and Barrels/OilBarrels.osg"), 
-}	
-Barrel2 =Transform{
-	position = {-9,0,-5.7},
-	--orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Storage and Barrels/OilBarrels.osg"), 
-}	
-Barrel3 =Transform{
-	position = {-9,0,-4.4},
-	--orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Storage and Barrels/OilBarrels.osg"), 
-}	
-Barrel4 =Transform{
-	position = {-7.7,0,-7},
-	--orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Storage and Barrels/OilBarrels.osg"), 
-}	
-Barrel5 =Transform{
-	position = {-7.7,0,-5.7},
-	--orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Storage and Barrels/OilBarrels.osg"), 
-}	
-Barrel6 =Transform{
-	position = {-7.7,0,-4.4},
-	--orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Storage and Barrels/OilBarrels.osg"), 
-}	
 
 
-Wrenches =Transform{
-	position = {-1,1,0},
-	--orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Tools/wrenches.osg"), 
-}	
-Diffuser1 =Transform{
-	position = {-3.2,6.5,-9},
-	--orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Structural/HVACdiffuser.osg"), 
-}	
-Diffuser2 =Transform{
-	position = {-3.2,6.5,-4},
-	--orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Structural/HVACdiffuser.osg"), 
-}	
-Diffuser3 =Transform{
-	position = {-3.2,6.5,1},
-	--orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Structural/HVACdiffuser.osg"), 
-}	
-Diffuser4=Transform{
-	position = {3.7,6.5,-9},
-	--orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Structural/HVACdiffuser.osg"), 
-}
-Diffuser5 =Transform{
-	position = {3.7,6.5,-4},
-	--orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Structural/HVACdiffuser.osg"), 
-}	
-Diffuser6 =Transform{
-	position = {3.7,6.5,1},
-	--orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Structural/HVACdiffuser.osg"), 
-}
-Diffuser7=Transform{
-	position = {10.6,6.5,-9},
-	--orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Structural/HVACdiffuser.osg"), 
-}
-Diffuser8 =Transform{
-	position = {10.6,6.5,-4},
-	--orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Structural/HVACdiffuser.osg"), 
-}	
-Diffuser9 =Transform{
-	position = {10.6,6.5,1},
-	--orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Structural/HVACdiffuser.osg"), 
-	}
 
-Beam1=Transform{
-	position = {-1.6,0,-2},
-	--orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Structural/beam.osg"), 
-}
-Beam2=Transform{
-	position = {-1.6,0,-9},
-	--orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Structural/beam.osg"), 
-}
-Beam3=Transform{
-	position = {8.4,0,-9},
-	--orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Structural/beam.osg"), 
-}
-Beam4=Transform{
-	position = {8.4,0,-2},
-	--orientation = AngleAxis(Degrees(-90), Axis{0,1,0}),
-	Model("Factory Models/OSG/Structural/beam.osg"), 
-}
+
+
 RelativeTo.World:addChild(factory)
 factory:addChild(room)	
 factory:addChild(milling)
 --factory:addChild(Lathe)
 factory:addChild(robot)
 factory:addChild(robot2)
---factory:addChild(robot3)
+
 factory:addChild(rollerGroup)
 factory:addChild(rollerGroup2)
 factory:addChild(palletGroup)
 factory:addChild(HVACGroup)
-
+factory:addChild(DiffuserGroup)
+factory:addChild(WorkbenchGroup)
+factory:addChild(BeamGroup)
+factory:addChild(BarrelGroup)
 --factory:addChild(cnc)
 factory:addChild(StorageBinBlue)
 factory:addChild(StorageBinRed)
@@ -493,13 +463,8 @@ factory:addChild(StorageBinYellow)
 --factory:addChild(StorageBinGreen)
 factory:addChild(forklift)
 factory:addChild(Compressor)
-factory:addChild(Workbench)
-factory:addChild(Workbench2)
-factory:addChild(Workbench3)
-factory:addChild(Workbench4)
-factory:addChild(Workbench5)
-factory:addChild(Workbench6)
-factory:addChild(Workbench7)
+
+
 
 factory:addChild(ToolBox)
 factory:addChild(Chair)
@@ -516,27 +481,8 @@ factory:addChild(GarageDoor1)
 factory:addChild(GarageDoor2)
 
 --factory:addChild(ForkliftPath)
-factory:addChild(Barrel)
-factory:addChild(Barrel2)
-factory:addChild(Barrel3)
-factory:addChild(Barrel4)
-factory:addChild(Barrel5)
-factory:addChild(Barrel6)
-
-Workbench3:addChild(Wrenches)
-
-factory:addChild(Diffuser1)
-factory:addChild(Diffuser2)
-factory:addChild(Diffuser3)
-factory:addChild(Diffuser4)
-factory:addChild(Diffuser5)
-factory:addChild(Diffuser6)
-factory:addChild(Diffuser7)
-factory:addChild(Diffuser8)
-factory:addChild(Diffuser9)
 
 
-factory:addChild(Beam1)
-factory:addChild(Beam2)
-factory:addChild(Beam3)
-factory:addChild(Beam4)
+
+
+
