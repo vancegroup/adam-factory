ec53a5331034c5b8abec7dc287a59115c6e96de7
unknown <adamc17@metal-render.vrac.iastate.edu>
2012-05-02 16:45:03 -0500

    Added light to VRjuglua script

--- Factory/vrj_loadFactory.lua
+++ Factory/vrj_loadFactory.lua
@@ -1,4 +1,4 @@
 require("getScriptFilename")
 vrjLua.appendToModelSearchPath(getScriptFilename())
-
+dofile(vrjLua.findInModelSearchPath([[simpleLights.lua]]))
 RelativeTo.World:addChild(dofile(vrjLua.findInModelSearchPath("Factory.lua")))
