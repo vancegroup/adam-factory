4c493020b396770c1600094bd58b489f7339e7a8
Ryan Pavlik <rpavlik@iastate.edu>
2013-05-06 10:17:12 -0500

    run optimizer

--- Factory/loadIntoSparta.lua
+++ Factory/loadIntoSparta.lua
@@ -1,4 +1,9 @@
 require("getScriptFilename")
 vrjLua.appendToModelSearchPath(getScriptFilename())
-
-SpartaAppDelegate:replaceRoomModel(dofile(vrjLua.findInModelSearchPath("Factory.lua")))
+local newroom = Transform{
+	position = {2.5, 0, 2.5},
+	dofile(vrjLua.findInModelSearchPath("Factory.lua"))
+}
+local optim = osgUtil.Optimizer()
+optim:optimize(newroom)
+SpartaAppDelegate:replaceRoomModel(newroom)
