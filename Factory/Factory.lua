ae2ce0b33a67f6a05f6eb0531781ed5fbbbd75ec
Andrew Moore <admoore@iastate.edu>
2013-08-13 12:26:09 -0500

    Change initial orientation of forklift.

--- Factory/Factory.lua
+++ Factory/Factory.lua
@@ -496,7 +496,7 @@ Ladder = Transform{
 
 forklift = Transform {
 	position = {7.5, 0, .5},
-	orientation = AngleAxis(Degrees(0), Axis{0, 1, 0}),
+	orientation = AngleAxis(Degrees(-90), Axis{0, 1, 0}),
 	Model[[Factory Models/OSG/Shop Carts and Fork Lifts/Forklift.osg]]
 }
 RelativeTo.World:addChild(forklift)
