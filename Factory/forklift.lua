39c17cb2464ccf06cc4995a5e5056e32e51ac9b6
Leif Berg <lpberg@iastate.edu>
2013-10-11 15:37:52 -0500

    fixed forklift navigation

--- Factory/forklift.lua
+++ Factory/forklift.lua
@@ -1,5 +1,18 @@
 require("Actions")
 
+local getRoomToWorld = function()
+	return RelativeTo.World:getInverseMatrix()
+end
+
+local transformPointRoomToWorld = function(v)
+	return getRoomToWorld():preMult(v)
+end
+
+local transformPointWorldToRoom = function(v)
+	return RelativeTo.World:getMatrix():preMult(v)
+end
+
+
 local forklift = Transform {
 	position = {0, 0, 0 },
 	orientation = AngleAxis(Degrees(-90), Axis{0, 1, 0}),
@@ -15,12 +28,28 @@ Actions.addFrameAction(
 			repeat
 				Actions.waitForRedraw()
 			until device.justPressed
+			--get forklifts position (currently in room)
+			local room_pos = forklift:getPosition()
+			--remove forlift from room
 			RelativeTo.Room:removeChild(forklift)
+			--"transform" the forklifts position with respect to the world
+			local world_pos = transformPointRoomToWorld(room_pos)
+			--update the position of the forklift to the world position
+			forklift:setPosition(world_pos)
+			--add forklift to world
 			RelativeTo.World:addChild(forklift)
 			repeat
 				Actions.waitForRedraw()
 			until device.justPressed
+			--get forklifts position (currently in the world)
+			local world_pos = forklift:getPosition()
+			--remove the forklift from the world
 			RelativeTo.World:removeChild(forklift)
+			--calculate room position with respect to world
+			local room_pos = transformPointWorldToRoom(world_pos)
+			--	--update the position of the forklift to the room position
+			forklift:setPosition(room_pos)
+			--add forklift to room
 			RelativeTo.Room:addChild(forklift)
 		end
 	end
