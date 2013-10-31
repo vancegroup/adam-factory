f9ee469216ede738b35ec9ad03fc0cad40d6632c
Leif Berg <lpberg@iastate.edu>
2013-10-31 14:40:55 -0500

    Update forklift.lua
    changed forklift to be a MT over a PAT. Applied inverse matrix to account for both position and rotation in moving the forklift from world to room and back again.

--- Factory/forklift.lua
+++ Factory/forklift.lua
@@ -8,12 +8,16 @@ local transformPointRoomToWorld = function(v)
 	return getRoomToWorld():preMult(v)
 end
 
+local transformMatrixRoomToWorld = function(v)
+	return getRoomToWorld():preMult(v)
+end
+
 local transformPointWorldToRoom = function(v)
 	return RelativeTo.World:getMatrix():preMult(v)
 end
 
 
-local forklift = Transform {
+local forklift = MatrixTransform{
 	position = {0, 0, 0 },
 	orientation = AngleAxis(Degrees(-90), Axis{0, 1, 0}),
 	Model[[Factory Models/OSG/Shop Carts and Fork Lifts/Forklift.osg]]
@@ -29,26 +33,26 @@ Actions.addFrameAction(
 				Actions.waitForRedraw()
 			until device.justPressed
 			--get forklifts position (currently in room)
-			local room_pos = forklift:getPosition()
+			local room_pose = forklift:getMatrix()
 			--remove forlift from room
 			RelativeTo.Room:removeChild(forklift)
 			--"transform" the forklifts position with respect to the world
-			local world_pos = transformPointRoomToWorld(room_pos)
+			local world_pose = transformMatrixRoomToWorld(room_pose)
 			--update the position of the forklift to the world position
-			forklift:setPosition(world_pos)
+			forklift:setMatrix(world_pose)
 			--add forklift to world
 			RelativeTo.World:addChild(forklift)
 			repeat
 				Actions.waitForRedraw()
 			until device.justPressed
 			--get forklifts position (currently in the world)
-			local world_pos = forklift:getPosition()
+			local world_pose = forklift:getMatrix()
 			--remove the forklift from the world
 			RelativeTo.World:removeChild(forklift)
 			--calculate room position with respect to world
-			local room_pos = transformPointWorldToRoom(world_pos)
+			local room_pose = transformMatrixRoomToWorld(world_pose)
 			--	--update the position of the forklift to the room position
-			forklift:setPosition(room_pos)
+			forklift:setMatrix(room_pose)
 			--add forklift to room
 			RelativeTo.Room:addChild(forklift)
 		end
